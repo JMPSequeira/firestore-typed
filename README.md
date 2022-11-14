ab67d8ab16fd7d005759e0821b908cd123bbaa7f
João Sequeira <email.sequeira@gmail.com>
2022-11-14 13:53:16 +0000

    refactored

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # StrongStore 
-(formerly *firestore-typed*)
 
 &nbsp;
 ## What it is?
@@ -27,10 +26,10 @@ ______________
 __________
 &nbsp;
 
-StrongStore redeclares functions in 'firebase/firestore' so if you have --skipLibCheck false it will throw a redeclaration error for each of one of those functions. 
+StrongStore redeclares functions in 'firebase/firestore' so if you compile with '--skipLibCheck false' it will throw a redeclaration error for each of one of those functions. 
 
 &nbsp;
-- Import it into a declaration (d.ts) file:
+- Import StrongStore into a declaration (d.ts) file (I personally call it data.d.ts):
     ```ts
     import "strongstore";
     ```
@@ -39,11 +38,12 @@ StrongStore redeclares functions in 'firebase/firestore' so if you have --skipLi
 ### Modes
 &nbsp;
 
-StrongStore works in two mutually exclusive ways: Strict or Typed.
+StrongStore works in two mutually exclusive ways: Typed or Strict.
 
 &nbsp;
-#### Typed Mode
+#### Typed Mode (*No extra configuration needed*)
 &nbsp;
+
 - Simply use firebase as you'd normally do:
     ```ts
     import { collection, getDocs, getFirestore, orderBy, query, where } from 'firebase/firestore';
@@ -55,40 +55,91 @@ StrongStore works in two mutually exclusive ways: Strict or Typed.
         interface Person { name:string; nicknames: string[]; } <- a nested object
     */
     
-    import { City } from './models/city';
+    import { City } from '/models/city';
     ```
-- Enjoy the range the features provided: 
-    <img src="./images/typed-mode.png" alt="drawing" width="1000"/>
+- Enjoy the features provided: <img src="https://github.com/JMPSequeira/strongstore/blob/master/images/typed-mode.png?raw=true" alt="drawing" width="1000"/>
+
+&nbsp;
+
+#### Strict Mode
+&nbsp;
+Strict Mode relies on a declared global type called FirestoreConfig. If it does not exist it defaults to Typed Mode. 
+Strict Mode is ...strict. Only configured collections will work.
+- Import your data models in the same file you imported StrongStore:
+    ```ts
+    /* 
+        interface City { name:string; }
+
+        interface Attraction { type: "Park" | "Station" | "Monument"}
+    */
     
-- Declare you firestore paths 
+    import { City } from '/models/city';
+    import { Attraction } from '/models/attraction';
+    ```
+- Declare FirestoreConfig in global declaration:
     ```ts
     declare global {
-        type FirestorePaths = {
+        type FirestoreConfig = {
             cities: City;
-            ["cities/cityCenters"]: CityCenter; // <- to express a sub collection
+            ["cities/attractions"]: Attraction; // <- to express a sub collection
         }
     }
     ```
-## Usage
 
-    
-- But now you'll enjoy based solely on the collection path:
-  - Autocomplete for paths:
-    
-    ![Path Autocomplete](https://github.com/JMPSequeira/strongstore/blob/master/images/autocomplete-filters.png?raw=true)
+- All features from Typed Mode will work but now instead of casting you'll pass a path to a collection or document and it will resolve a:
+  - MappedCollection<T, Key> (extends CollectionReference<T>) 
+  - or MappedDocument<T, Key> (extends DocumentReference<T>):
+  ```ts
+  const firestore = getFirestore();
 
-  - Autocomplete filters for property type:
-  
-    ![Filter Autocomplete](https://github.com/JMPSequeira/strongstore/blob/master/images/autocomplete-filters.png?raw=true)
+  //      ↓ MappedCollection<City, "cities">
+  const citiesCollection = collection(firestore,"cities");
+  //      ↓ MappedDocument<City, "cities">
+  const nyDoc = doc(firestore,"cities/NY");
+  ``` 
+- Besides the explicit path, you can use:
+  ```ts
+  //      ↓ MappedCollection<Attraction, "cities/attractions
+  const nyAttractions = collection(firestore,"cities", "NY", "attractions");
+  //      is the same as
+  const nyAttractions = collection(cities,"NY/attractions");
+  //      is the same as
+  const nyAttractions = collection(cities,"NY", "attractions");
+  //      is the same as
+  const nyAttractions = collection(cities,"NY/", "/attractions"); // <= trims leading and trailing '/'
+  //      is the same as
+  const nyAttractions = collection(nyDoc,"attractions");
+  ```
+- You can even use string templates, or variables, for path segments:
+  - For collection segments, as long as it's resolvable to a compile time constant:
+   ```ts
+  const citiesPath = "cities";
+  //      ↓ MappedCollection<Attraction, "cities/attractions
+  const nyAttractions = collection(firestore,citiesPath, "NY", "attractions");
+  //      is the same as
+  const nyAttractions = collection(firestore,`${citiesPath}/NY/attractions`);
+  ```
+- For document segments, as long as it's resolvable to a compile time constant, or it does not resolve to `${string | any | never | unknown}` (Unions are accepted as long as they follow the same pattern):
+  ```ts
+  function getNyCityAttractions(id: number) : Promise<QuerySnapshot<Attraction>> {
+    
+    //      ↓ MappedCollection<Attraction, "cities/attractions
+    const nyAttractions = collection(firestore,"cities", id, "attractions");
+    //      is the same as
+    const nyAttractions = collection(firestore,`cities/${id}/attractions`);
 
-  - Checks for the above:
+    return getDocs(nyAttractions);
+  }
+
+  function getNyCityAttractions(union: "NY" | "LA") : Promise<QuerySnapshot<Attraction>> {
     
-    ![Checking Paths](https://github.com/JMPSequeira/strongstore/blob/master/images/invalid-path.png?raw=true)
-    
-    ![Checking Filter](https://github.com/JMPSequeira/strongstore/blob/master/images/invalid-filter.png?raw=true)
-  
-  - Type checking for property values
-    
-    ![Checking Value](https://github.com/JMPSequeira/strongstore/blob/master/images/invalid-value.png?raw=true)
+    //      ↓ MappedCollection<Attraction, "cities/attractions
+    const nyAttractions = collection(firestore,"cities", union, "attractions");
+    //      is the same as
+    const nyAttractions = collection(firestore,`cities/${union}/attractions`);
+
+    return getDocs(nyAttractions);
+  }
+  ```
 
 
--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,8 @@
 
 import * as Firestore from "@firebase/firestore";
 
-//@ts-ignore
-interface QueryConstraint<T>
-    //@ts-ignore
-    extends Firestore.QueryConstraint {
+
+interface QueryConstraint<T> extends Firestore.QueryConstraint {
 }
 declare module "firebase/firestore" {
 
@@ -21,22 +19,19 @@ declare module "firebase/firestore" {
         : never
         ;
 
-    type MappedCollection<PS extends string[]> =
-        CollectionTypeFromSegments<PS> extends infer R
-        ? (
-            R extends Error
-            ? R
-            : MappedCollectionReference<R, Path<StrConcat<PS, "/">>>
-        )
+    type Mapped<PS extends string[], M extends PathType> =
+        TypeFromSegments2<PS> extends infer R
+        ? R extends Error ? R
+        : M extends "collection" ? MappedCollectionReference<R, StrConcat<ContractPath<PS>, "/">>
+        : MappedDocumentReference<R, StrConcat<ContractPath<PS>, "/">>
         : never
+
         ;
 
     interface MappedCollectionReference<T, P extends string>
         extends Firestore.CollectionReference<T> {
         parent: ParentDoc<P>
     }
-
-
     interface MappedDocumentReference<T, P extends string>
         extends Firestore.DocumentReference<T> {
         parent: MappedCollectionReference<T, P>
@@ -117,23 +112,8 @@ declare module "firebase/firestore" {
             , path: P
             , ...pathSegments: A
         )
-        : MappedCollection<[P, ...A]>;
-    type D = Path<StrConcat<["a", "cityCenters"], "/">, "document">;
-    function strictCollection
-        <
-            O
-            , P1 extends string
-            , P2 extends string
-            , A extends string[]
-            , PS extends Concat<Split<P1, "/">, Split<Path<StrConcat<[P2, ...A], "/">, "document">, "/">>
-            , R extends CollectionTypeFromSegments<PS>
-        >
-        (
-            reference: MappedCollectionReference<O, P1>
-            , path: P2
-            , ...pathSegments: A
-        )
-        : MappedCollection<PS>;
+        : Mapped<PreparePath<[P, ...A]>, "collection">;
+
 
     function strictCollection
         <
@@ -141,15 +121,33 @@ declare module "firebase/firestore" {
             , P1 extends string
             , P2 extends string
             , A extends string[]
-            , PS extends Concat<Split<P1, "/">, Split<Path<StrConcat<[P2, ...A], "/">>, "/">>
-            , R extends CollectionTypeFromSegments<PS>
+            , PS extends MergeSegments<P1, "collection", [P2, ...A], "collection">
+        >
+        (
+            reference: MappedCollectionReference<O, P1>
+            , path: P2
+            , ...pathSegments: A
+        )
+        //@ts-ignore
+        : Mapped<PS, "collection">;
+
+
+    function strictCollection
+        <
+            O
+            , P1 extends string
+            , P2 extends string
+            , A extends string[]
+            , PS extends MergeSegments<P1, "document", [P2, ...A], "collection">
         >
         (
             reference: MappedDocumentReference<O, P1>
             , path: P2
             , ...pathSegments: A
         )
-        : MappedCollection<PS>;
+
+        //@ts-ignore
+        : Mapped<PS, "collection">;
 
 
 
@@ -157,13 +155,12 @@ declare module "firebase/firestore" {
         <
             P extends string
             , A extends string[]
-            , T = DocumentTypeFromSegments<[P, ...A]>
         >(
             firestore: Firestore.Firestore
             , path: P
             , ...pathSegments: A
         )
-        : MappedDocumentReference<T, Path<StrConcat<[P, ...A], "/">, "document">>;
+        : Mapped<PreparePath<[P, ...A]>, "document">;
 
     function strictDocument
         <
@@ -171,15 +168,15 @@ declare module "firebase/firestore" {
             , P1 extends string
             , P2 extends string
             , A extends string[]
-            , PS extends Concat<Split<P1, "/">, Split<Path<StrConcat<[P2, ...A], "/">>, "/">>
-            , R extends DocumentTypeFromSegments<PS>
+            , PS extends MergeSegments<P1, "collection", [P2, ...A], "document">
         >
         (
-            reference: MappedDocumentReference<O, P1>
+            reference: MappedCollectionReference<O, P1>
             , path: P2
             , ...pathSegments: A
         )
-        : MappedDocumentReference<R, Path<StrConcat<PS, "/">>>;
+        //@ts-ignore
+        : Mapped<PS, "document">
 
     function strictDocument
         <
@@ -187,21 +184,21 @@ declare module "firebase/firestore" {
             , P1 extends string
             , P2 extends string
             , A extends string[]
-            , PS extends Concat<Split<P1, "/">, Split<Path<StrConcat<[P2, ...A], "/">, "document">, "/">>
-            , R extends DocumentTypeFromSegments<PS>
+            , PS extends MergeSegments<P1, "document", [P2, ...A], "document">
         >
         (
             reference: MappedDocumentReference<O, P1>
             , path: P2
             , ...pathSegments: A
         )
-        : MappedDocumentReference<R, Path<StrConcat<PS, "/">>>;
+        //@ts-ignore
+        : Mapped<PS, "document">
 
     //@ts-ignore
-    type Collection = Equals<FirestorePaths, any> extends false ? typeof strictCollection : typeof typedCollection;
+    type Collection = Equals<FirestoreConfig, any> extends false ? typeof strictCollection : typeof typedCollection;
 
     //@ts-ignore
-    type Doc = Equals<FirestorePaths, any> extends false ? typeof strictDocument : typeof typedDocument;
+    type Doc = Equals<FirestoreConfig, any> extends false ? typeof strictDocument : typeof typedDocument;
 
     //@ts-ignore
     const doc: Doc;
@@ -223,6 +220,8 @@ declare module "firebase/firestore" {
     //@ts-ignore
     const query: <T>(query: Firestore.Query<T>, ...queryConstraints: QueryConstraint<NoInfer<T>>[]) => Firestore.Query<T>;
 }
+type MergeSegments<P1 extends string, M1 extends PathType, P2 extends string[], M2 extends PathType> =
+    SegmentsFromParts<[...ExpandPath<Split<P1, "/">, M1>, ...P2], M2>;
 
 type Split<T extends string, S extends string> =
     T extends `${infer L}${S}${infer R}`
@@ -240,21 +239,9 @@ type PatternArray<P extends unknown[], T extends unknown[], AllowEmpty extends 0
     : T extends [...P, ...infer R] ? PatternArray<P, R, 1> extends never ? never
     : T : never;
 
+type PathType = "collection" | "document";
 
-type PatternArrayNoAny<P extends unknown[], T extends unknown[], AllowEmpty extends 0 | 1 = 0> = HasAny<T> extends true ? never : PatternArray<P, T, AllowEmpty>;
-
-type HasAny<T extends unknown[]> = T['length'] extends 0 ? false :
-    (any extends T[0] ? true : false) extends true
-    ? true
-    : HasAny<Shift<T>>;
-
-type Append<W, T extends unknown[]> = [...T, W];
-
-type Concat<W extends unknown[], T extends unknown[]> = [...W, ...T];
-
-type Prepend<W, T extends unknown[]> = [W, ...T];
-
-type StrConcat<T extends string[] | string, S extends string> =
+type StrConcat<T extends unknown[] | unknown, S extends string> =
     T extends [infer I, ...infer Rest] ?
     //@ts-ignore
     `${I}${StrConcat<Rest, S> extends infer P ? P extends "" ? P : `${S}${P}` : P}` :
@@ -262,13 +249,9 @@ type StrConcat<T extends string[] | string, S extends string> =
     ;
 
 //@ts-ignore
-type FirestoreKeys = unknown extends FirestorePaths ? string : keyof FirestorePaths;
+type FirestoreKeys = keyof FirestoreConfig;
 
 type AnyDocument = Firestore.DocumentData;
-
-type FirestoreType<P> = string extends P ? AnyDocument :
-    //@ts-ignore
-    P extends FirestoreKeys ? FirestorePaths[P] : AnyDocument;
 
 type NoInfer<T> = T extends infer S ? S : never;
 
@@ -333,24 +316,20 @@ type Invalid<M> = Error & M;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type _ = any;
 
-type PathType<T extends string, M extends Mode = "collection", C = 0> =
+type TypeOfPath<T extends string, M extends PathType = "collection", C = 0> =
     (C extends 100
         ? Invalid<"Maximum sub-collection depth is 100">
         : (T extends `${_}/${infer R}`
-            ? PathType<R, Switch<M>, Inc<C>> : M)
+            ? TypeOfPath<R, Switch<M>, Inc<C>> : M)
     );
 
-type Switch<T extends Mode> = T extends "collection" ? "document" : T extends "document" ? "collection" : never;
-
-type Mode = "collection" | "document";
-
-type CollectionPath<T extends string> = PathType<T> extends "collection" ? Path<T> : Invalid<"Path represents a document">;
+type Switch<T extends PathType> = T extends "collection" ? "document" : T extends "document" ? "collection" : never;
 
 
 type IfEqual<T, U, R = true, E = false> =
     Equals<T, U> extends true ? R : E;
 
-type Path<T extends string, M extends Mode = 'collection'> =
+type Path<T extends string, M extends PathType = 'collection'> =
     (T extends `${infer L}/${infer R}`
         ? (M extends "collection" ? `${L}${Path<R, Switch<M>> extends infer R ? R extends "" ? R : `/${R & string}` : never}` : `${Path<R, Switch<M>>}`)
         : (M extends "document" ? "" : T))
@@ -358,22 +337,83 @@ type Path<T extends string, M extends Mode = 'collection'> =
 
 
 //@ts-ignore
-type TypeFromPath<T extends string> = T extends keyof FirestorePaths ? FirestorePaths[T] : never;
+type TypeFromPath<T extends string> = T extends FirestoreKeys ? FirestoreConfig[T] : never;
 
-type CollectionTypeFromSegments<S extends string[]> = TypeFromSegments<S, "collection">;
-
-type DocumentTypeFromSegments<S extends string[]> = TypeFromSegments<S, "document">;
-
-type TypeFromSegments<S extends string[], M extends "collection" | "document"> = StrConcat<S, "/"> extends infer P
-    ? (P extends string
-        ?
-        (Or<StrContains<P, string>, StrContains<P, `${any}`>> extends false
-            ? ((PathType<P> extends M
-                ? IfEqual<TypeFromPath<Path<P>>, never, Invalid<`'${P}' is not a valid path`>, TypeFromPath<Path<P>>>
-                : Invalid<`Path '${P}' refers to ${Switch<M>}. Expecting a path for a ${M}`>))
-            : Invalid<`'${P}' is not a valid path`>)
-        : never
+type NoIncorrectLength<S extends string[], M extends PathType> =
+    Even<S['length']> extends true
+    ? (
+        M extends "collection"
+        ? Invalid<`Collection must have an odd number of segments. ${S['length']}`>
+        : S
     )
+    : (
+        M extends "document"
+        ? Invalid<`Document must have an even number of segments. ${S['length']}`>
+        : S
+    )
+
+    ;
+type NoDoubleSlash<S extends string[], N extends number = 0> = N extends S['length'] ? S :
+    S[N] extends (`${any}//${any}` | `//${any}` | `${any}//`) ? Invalid<`Segment at index ${N} contains invalid '//'`> : NoDoubleSlash<S, Inc<N>>;
+
+type NoInvalidPlaceholders<S extends string[], N extends number = 0> =
+    N extends S['length'] ? S :
+    HasInvalidPlaceholder<S[N]> extends true
+    ? Invalid<`Segment at index ${N} has an invalid placeholder (string | any | unknown)`>
+    : NoInvalidPlaceholders<S, Inc<N>>
+    ;
+type NoMoreThan100Parts<S extends string[], N extends number = 0> =
+    N extends 100 ? Invalid<`Contains more than 100 segments`>
+    : (
+        S['length'] extends N
+        ? S
+        : NoMoreThan100Parts<S, Inc<N>>
+    );
+
+
+type HasInvalidPlaceholder<S extends string> =
+    S extends `${infer P}${infer R}`
+    ? (
+        Or<Equals<P, `${string}`>
+            , Or<Equals<P, `${any}`>
+                //@ts-ignore
+                , Equals<P, `${unknown}`>>> extends true
+        ? true
+        : HasInvalidPlaceholder<R>
+    )
+    : Or<Equals<S, `${string}`>
+        , Or<Equals<S, `${any}`>
+            //@ts-ignore
+            , Equals<S, `${unknown}`>>>;
+type SegmentsFromParts<S extends string[], M extends PathType> =
+    NoDoubleSlash<S> extends infer MaybeError ? MaybeError extends Error ? MaybeError
+    : NoInvalidPlaceholders<S> extends infer MaybeError ? MaybeError extends Error ? MaybeError
+    : PreparePath<S> extends infer Segments
+    // ? Segments 
+    //@ts-ignore
+    ? NoMoreThan100Parts<Segments> extends infer MaybeError ? MaybeError extends Error ? MaybeError
+    //@ts-ignore
+    : NoIncorrectLength<Segments, M>
+    : never
+    : never
+    : never
+    : never
+    // : never
+    ;
+
+type T0 = SegmentsFromParts<["cities", "NY" | "LA"], "document">;
+declare global {
+    type FirestoreConfig = {
+        "cities": number
+    }
+}
+
+type TypeFromSegments2<S extends unknown[]> =
+    StrConcat<ContractPath<S>, "/"> extends infer K
+    ? K extends FirestoreKeys
+    //@ts-ignore
+    ? FirestoreConfig[K]
+    : Invalid<`Can not find '${K & string}' in the provided paths`>
     : never
     ;
 
@@ -381,13 +421,46 @@ type And<L, R> = L extends true ? R extends true ? true : false : false;
 
 type Or<L, R> = L extends true ? true : R extends true ? true : false;
 
-type StrContains<T, P> = T extends `${infer A}${infer R}`
-    ? IfEqual<A, P, true, StrContains<R, P>>
-    : IfEqual<T, P>
-    ;
-
 type Equals<T, U> =
     (<V>() => V extends T ? 1 : 2) extends (<V>() => V extends U ? 1 : 2)
     ? true
     : false
     ;
+
+type Even<T extends number> = `${T}` extends `${string}${0 | 2 | 4 | 6 | 8}` ? true : false;
+
+type Odd<T extends number> = `${T}` extends `${string}${1 | 3 | 5 | 7 | 9}` ? true : false;
+
+type DocMark = `📄`;
+
+type PreparePath<S extends unknown[]> = PartsToSegment<S> extends infer Segments ?
+    //@ts-ignore
+    [...
+        {
+            [K in keyof Segments]: K extends `${number}`
+            ? (
+                // @ts-ignore
+                Odd<K> extends true
+                ? DocMark
+                : Segments[K]
+            )
+            : never;
+        }] : never;
+
+type ExpandPath<S extends unknown[], M extends "collection" | "document"> =
+    S extends "" ? "" : S extends [infer O] ? M extends "document" ? [O, DocMark] : [O] : S extends [infer A, ...infer R] ?
+    [A, DocMark, ...ExpandPath<R, M>] : S
+    ;
+
+
+type ContractPath<S extends unknown[]> = S extends [] ? [] : S extends [infer A, ...infer R] ?
+    A extends DocMark ? ContractPath<R> : [A, ...ContractPath<R>] : S;
+
+
+
+
+type PartsToSegment<S extends unknown[]> = S extends [] ? [] : S extends [infer A, ...infer R] ?
+    //@ts-ignore
+    [...Split<RemoveEdgeSlashes<A>, "/">, ...PartsToSegment<R>] : S;
+
+type RemoveEdgeSlashes<S extends string> = S extends (`/${infer D}/` | `/${infer D}` | `${infer D}/`) ? RemoveEdgeSlashes<D> : S;
